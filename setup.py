d9fb5200b6fe192175fc70674b148a92d524a293
mrMekdad <mekdadr6@gmail.com>
2022-01-22 06:57:00 +0100

    metabolomics-merge-tools: add repository metadata and packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="reda-2022-metabolomics-merge-tools",
+    version="0.1.0",
+    description="Utilities to merge sample sheets, metabolite matrices, and curated panel labels.",
+    author="Red@",
+    packages=["metabolomics_merge_tools"],
+    package_dir={"": "src"},
+)
